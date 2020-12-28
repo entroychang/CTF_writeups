fcfcb3d08e8495188549a475ccb3fc9dd14c734f
entroy0421 <39701965a@gmail.com>
2020-12-28 20:09:29 +0800

    commit

--- SWCTF/逢甲-Crypto/Base-XX_script.py
+++ SWCTF/逢甲-Crypto/Base-XX_script.py
@@ -1,4 +1,4 @@
 c = 'ZIpR1J2NvPqtgSJxbO4xnJ1JTGotDRpxoO4ZnLoJiMqxYAIt5TE++'
+base_64 = 'wpZTV0NURntCYXNlNjRfdEhlX0xhTkdVNDZlX09mX2g0Q2szcnN9Cg=='
 
-for i in c:
-    print(chr(ord(i) + 18))
+print(len(base_64))
